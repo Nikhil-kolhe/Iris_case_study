45a50f07874019546c0beb558e3dfe8233dba45b
Nikhil kolhe <92266685+Nikhil-kolhe@users.noreply.github.com>
2023-02-01 19:04:39 +0530

    Add files via upload

---
+++ Iriscasestudy2.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+def DecisionTreeClassifierX():
+    Dataset = load_iris()       # 1 Load the data
+    
+    Data = Dataset.data
+    Target = Dataset.target
+
+    # 2 Manipulate the data
+    Data_train, Data_test, Target_train, Target_test = train_test_split(Data,Target,test_size=0.5)
+    
+    Classifier = DecisionTreeClassifier()
+    
+    # 3 Build the model
+    Classifier.fit(Data_train,Target_train)
+    
+    # 4 Test the model 
+    Predictions = Classifier.predict(Data_test)
+    
+    Accuracy = accuracy_score(Target_test,Predictions)
+    
+    # 5 Improve :- missing
+    
+    return Accuracy
+    
+    
+def main():
+    Ans = DecisionTreeClassifierX()
+    
+    print(f"Accuracy of Iris dataset with KNN is {Ans * 100}")
+    
+if __name__ == "__main__":
+    main()    
+    
+    
